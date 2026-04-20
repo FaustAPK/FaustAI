0ccf8bbb0ea383cabd943ccb379d89036841c9af
FaustAPK <rblum1208@gmail.com>
2026-04-20 03:59:44 +0200

    Update MainApplication.kt

--- android/app/src/main/java/com/pocketpalai/MainApplication.kt
+++ android/app/src/main/java/com/pocketpalai/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.pocketpal
+package com.faust.ai // Изменён пакет
 
 import android.app.Application
 import com.facebook.react.PackageList
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+// Импорты кастомных пакетов (они остались в старом пакете com.pocketpal)
 import com.pocketpal.KeepAwakePackage
 import com.pocketpal.HardwareInfoPackage
 import com.pocketpal.StorefrontPackage
@@ -18,35 +19,35 @@ import com.pocketpal.download.DownloadPackage
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-              add(KeepAwakePackage())
-              add(HardwareInfoPackage())
-              add(StorefrontPackage())
-              add(DownloadPackage())
-            }
+    override val reactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
+            override fun getPackages(): List<ReactPackage> =
+                PackageList(this).packages.apply {
+                    // Пакеты, которые не могут быть автоматически подключены, можно добавить вручную здесь, например:
+                    // add(MyReactNativePackage())
+                    add(KeepAwakePackage())
+                    add(HardwareInfoPackage())
+                    add(StorefrontPackage())
+                    add(DownloadPackage())
+                }
 
-        override fun getJSMainModuleName(): String = "index"
+            override fun getJSMainModuleName(): String = "index"
 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        }
 
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
-  override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, OpenSourceMergedSoMapping)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // Если вы выбрали Новую Архитектуру, загружаем нативную точку входа для этого приложения.
+            load()
+        }
     }
-  }
 }
