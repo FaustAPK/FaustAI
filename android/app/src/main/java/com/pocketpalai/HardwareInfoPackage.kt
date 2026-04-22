98aa7d7e131df4f2030dc628c9a2d6dbcca90fdd
FaustAPK <rblum1208@gmail.com>
2026-04-23 00:17:23 +0200

    Update HardwareInfoPackage.kt

--- android/app/src/main/java/com/pocketpalai/HardwareInfoPackage.kt
+++ android/app/src/main/java/com/pocketpalai/HardwareInfoPackage.kt
@@ -1,35 +1,16 @@
 package com.pocketpal
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.module.model.ReactModuleInfo
-import com.facebook.react.module.model.ReactModuleInfoProvider
-import com.pocketpal.specs.NativeHardwareInfoSpec
+import com.facebook.react.uimanager.ViewManager
 
-class HardwareInfoPackage : TurboReactPackage() {
-  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
-    return if (name == NativeHardwareInfoSpec.NAME) {
-      HardwareInfoModule(reactContext)
-    } else {
-      null
+class HardwareInfoPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(HardwareInfoModule(reactContext))
     }
-  }
 
-  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
-    return ReactModuleInfoProvider {
-      mapOf(
-        NativeHardwareInfoSpec.NAME to ReactModuleInfo(
-          NativeHardwareInfoSpec.NAME,
-          NativeHardwareInfoSpec.NAME,
-          false, // canOverrideExistingModule
-          false, // needsEagerInit
-          true,  // hasConstants
-          false, // isCxxModule
-          true   // isTurboModule
-        )
-      )
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
     }
-  }
 }
-
